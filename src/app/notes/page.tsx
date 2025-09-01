fb985ff1305796d5a1f3c21b30836e5708355d07
lucas mendes <lucasmndes.dev@gmail.com>
2025-09-01 13:48:27 -0300

    criando header com menu drawer da página de notes

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { NotesMenu } from "@/components/NotesMenu/notesMenu";
+import { useEffect, useState } from "react";
+import { FaBars, FaPlus } from "react-icons/fa";
+
+export default function Notes(){
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isDesktop, setIsDesktop] = useState(false);
+
+  // Detecta se é desktop (md: 768px ou maior)
+  useEffect(() => {
+    const checkScreen = () => setIsDesktop(window.innerWidth >= 768);
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
+
+    return (
+      <>
+        {!isDesktop && menuOpen && (
+          <div className="fixed inset-0 z-50">
+            <NotesMenu 
+            onClose={() => setMenuOpen(false)} 
+            isDesktop={isDesktop}/>
+            {/* Overlay para fechar ao clicar fora */}
+            <div
+              className="fixed inset-0 bg-black/40"
+              onClick={() => setMenuOpen(false)}
+            />
+          </div>
+        )}
+
+        {isDesktop && (
+          <NotesMenu onClose={() => {}} isDesktop={isDesktop} />
+        )}
+
+      <div
+      className={isDesktop ? "ml-56" : ""}>
+        <header className="bg-[var(--header)] flex justify-between items-center p-4 border-b-1 border-b-[var(--border)]">
+          <div className="flex gap-2 text-white items-center">
+            {
+              !isDesktop && (
+                  <button
+                  className="p-2 rounded hover:bg-[var(--chart2-secondary)] transition-colors cursor-pointer"
+                  onClick={() => setMenuOpen(true)}
+                  >
+                    <FaBars size={14} />
+                  </button>
+              )
+            }
+            <div className="flex flex-col">
+              <h1
+              className="text-lg font-bold">
+                Dashboard
+              </h1>
+              <p
+              className="text-sm text-gray-400">
+                Gerencie suas anotações e códigos
+              </p>
+            </div>
+          </div>
+          <button
+          className="bg-[var(--primary)] text-black hover:brightness-90 py-1 px-3 rounded cursor-pointer flex items-center gap-3">
+            <FaPlus size={14} />
+            <span
+              className="font-semibold text-[var(--chart5-primary)]"
+            >
+              Nova Nota
+            </span>
+          </button>
+        </header>
+        <main className="bg-[var(--background)] flex flex-col justify-center items-center"></main>
+      </div>
+      </>
+    );
+}
---
+++ src/components/NotesMenu/notesMenu.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { FaCode, FaPlus } from 'react-icons/fa'
+import { LuNotebookText } from "react-icons/lu";
+import { CiSettings } from "react-icons/ci";
+import { MdOutlineExitToApp } from "react-icons/md";
+
+interface NotesMenuProps {
+    onClose: () => void,
+    isDesktop: boolean
+}
+
+export const NotesMenu = ({ onClose, isDesktop }: NotesMenuProps) => {
+    return (
+        <aside className="fixed left-0 top-0 h-screen w-56 bg-[#1E293B] flex flex-col border-r border-[var(--border)] z-50">
+            <div className="flex justify-between items-center mb-6 mt-6 px-4 pb-2 border-b-1 border-b-[var(--border)]">
+                <div className="flex gap-2 text-[16px]">
+                    <FaCode className="text-[var(--primary)] mt-1" />
+                    <span className="font-bold text-[var(--foreground)]">
+                        CodeNotes
+                    </span>
+                </div>
+                { !isDesktop && (
+                    <button
+                        onClick={onClose}
+                        className="text-[var(--foreground)] text-lg px-2 py-1 rounded hover:bg-[var(--chart2-secondary)] transition-colors"
+                        aria-label="Fechar menu"
+                    >
+                        x
+                    </button>
+                )}
+            </div>
+            <nav className="flex flex-col h-full justify-between gap-4 px-4">
+                <div
+                className="flex flex-col">
+                    <div
+                    className="flex gap-3 px-3 py-2 items-center hover:bg-[var(--chart2-secondary)] rounded">
+                        <LuNotebookText className="text-[var(--foreground)]" />
+                        <a href="/notes" className="text-[var(--foreground)]">Minhas notas</a>
+                    </div>
+                    <div
+                    className='flex gap-3 px-3 py-2 items-center hover:bg-[var(--chart2-secondary)] rounded'>
+                        <FaPlus className="text-[var(--foreground)] " />
+                        <a href="/editor" className="text-[var(--foreground)]">Editor</a>
+                    </div>
+                    
+                </div>
+
+                <div
+                className="flex flex-col mb-6 border-t-1 border-t-[var(--border)]">
+                    <div
+                    className='flex gap-3 px-3 py-2 items-center hover:bg-[var(--chart2-secondary)] rounded'>
+                        <CiSettings className="text-[var(--foreground)]" />
+                        <a href="/settings" className="text-[var(--foreground)]">Configurações</a>
+                    </div>
+                    <div
+                    className='flex gap-3 items-center px-3 py-2 hover:bg-[var(--chart2-secondary)] rounded'>
+                        <MdOutlineExitToApp className="text-[var(--foreground)]" />
+                        <a href="/editor" className="text-[var(--foreground)]"> Sair </a>
+                    </div>
+                </div>
+
+            </nav>
+        </aside>
+    )
+}
